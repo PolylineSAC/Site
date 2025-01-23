0d4d75d2837c59017cdca1c5fd70c7a1fc20b977
PolylineSAC <PolylineSACInmobiliaria@gmail.com>
2025-01-23 07:12:15 -0500

    Delete js directory

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const userEmailElement = document.querySelector('.user-email');
-    if (userEmailElement) {
-        // Obtener el nombre del usuario del sessionStorage
-        const userName = sessionStorage.getItem('userName') || 'Usuario';
-        userEmailElement.textContent = userName;
-    }
-});
