237ab25341050798aba3e3aca4dfafc6db55078b
PolylineSAC <PolylineSACInmobiliaria@gmail.com>
2025-02-21 09:03:47 -0500

    assistant.js

--- js/assistant.js
+++ js/assistant.js
@@ -22,6 +22,11 @@ class VirtualAssistant {
                 type: 'whatsapp',
                 text: '💬 Chatear por WhatsApp',
                 url: 'https://wa.link/lkvfnb'
+            },
+            {
+                type: 'email',
+                text: '📧 Enviar un correo',
+                url: 'mailto:contacto@polyline.com'
             }
         ];
         this.isMuted = false;
